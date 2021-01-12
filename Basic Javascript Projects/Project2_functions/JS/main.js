bedc96488431af1f55bcc55559b8bde53f0b5ee5
GHam83 <greghamill@hotmail.com>
2021-01-11 23:08:06 -0800

    Push Java
    Push per pg 64

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date(); //function to display date when clicked
+}
+
+function myFunction(){
+    var sentence = "I am learning";
+    sentence += "a lot from this assignment";
+    document.getElementById("concatenate").innerHTML = sentence; //function to display string when clicked
+}

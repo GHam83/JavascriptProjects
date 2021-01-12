b494dedc4c4128b3806c5e9340c302b31f040432
GHam83 <greghamill@hotmail.com>
2021-01-11 22:16:49 -0800

    Push pg59
    Push Javascript pg 59

---
+++ Basic Javascript Projects/Project1_expressions_alert/Project1_expressions/main.js
@@ -0,0 +1,19 @@
+x= "\"What are you doing?\" \"I'm writing in javascript.\"" //making a variable
+y= "doh my" //creating var y
+window.alert(x); //making  window alert that shows "x"
+
+document.write(x + y) //having document write phrase
+
+var Pets = " Dogs ", Spaniel= " Farkie ", Bassit="Andy", Beagle=" Sneaky ", Terrier="Happy", Birddog="Pepper", Dingo=" Man Pup "; //multiple variables created
+var Dingo=Dingo.fontcolor("green"); //set color Dingo
+var Pets=Pets.fontcolor("red"); //set color Pets
+var Spaniel=Spaniel.fontcolor("yellow");//set color Spaniel
+var Beagle=Beagle.fontcolor("orange");// set color Beagle
+document.write(Dingo); //have document print colored var
+document.write(Pets);//have document print colored var
+document.write(Beagle);//have document print colored var
+document.write(Spaniel);//have document print colored var
+
+document.write(3+3);//expression
+
+

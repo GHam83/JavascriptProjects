6430a694786b2290753bbda4ef50fa6d5af402b8
GHam83 <greghamill@hotmail.com>
2021-01-17 19:21:14 -0800

    Push pg 116 Java
    Push per pg 116 instructions

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,52 @@
+document.write(typeof "word");
+
+document.write(2E319);
+
+document.write(-2E319);
+
+document.write(10>3);
+
+document.write(100>1000);
+
+console.log(2+7);
+
+document.write("12" + 3);
+
+console.log(2>7);
+
+document.write(3==-3);
+
+document.write(3==4);
+
+A=8
+B=8
+document.write(A===B);
+
+C="Dude"
+D="Gal"
+document.write(C===D);
+
+E="Hi"
+F=6
+document.write(E===F);
+
+Parent = "Mom"
+Sibling = "Sister"
+document.write(Parent===Sibling);
+
+document.write(3>1 && 8>7);
+
+document.write(3>1 && 3>8);
+
+document.write(2>3 || 2>4);
+
+document.write(2>1 || 2>3);
+
+function not_function() {
+    document.getElementById("Not").innerHTML = !(20>10);
+}
+
+function not_function2() {
+    document.getElementById("Not2").innerHTML = !(5>10);
+}
+
